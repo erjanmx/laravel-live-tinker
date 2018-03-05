c51a523f317226271cb891b3867750ca0b178b2b
erjanmx <erjanmx@gmail.com>
2018-03-05 16:13:33 +0300

    Add settings

--- src/Commands/LiveTinkerCommand.php
+++ src/Commands/LiveTinkerCommand.php
@@ -8,6 +8,14 @@ use Illuminate\Console\Command;
 
 class LiveTinkerCommand extends Command
 {
+    protected $webSocketIp;
+
+    protected $webSocketPort;
+
+    protected $webServerIp;
+
+    protected $webServerPort;
+
     /**
      * The name and signature of the console command.
      *
@@ -30,21 +38,28 @@ class LiveTinkerCommand extends Command
     public function __construct()
     {
         parent::__construct();
+
+        $this->webServerIp = env('LIVE_TINKER_SOCKET_IP', '0.0.0.0');
+        $this->webServerPort = env('LIVE_TINKER_SOCKET_PORT', '2345');
+
+        $this->webSocketIp = env('LIVE_TINKER_SERVER_IP', '0.0.0.0');
+        $this->webSocketPort = env('LIVE_TINKER_SERVER_PORT', '2346');
     }
 
     public function initWorkers()
     {
-        $ws = new Worker("websocket://0.0.0.0:2346");
-        $server = new WebServer("http://0.0.0.0:2345");
+        $ws = new Worker("websocket://{$this->webSocketIp}:{$this->webSocketPort}");
+        $server = new WebServer("http://{$this->webServerIp}:{$this->webServerPort}");
 
         $ws->onMessage = function($connection, $data) {
             // using shell_exec to get code's live state
+            // maybe set in config, either exec or native artisan call
             $output = shell_exec("php artisan live-tinker '$data'");
 
             $connection->send($output ?? '');
         };
 
-        $server->addRoot('127.0.0.1:2345', 'laravel-live-tinker/public');
+        $server->addRoot("{$this->webServerIp}:{$this->webServerPort}", 'laravel-live-tinker/public');
 
         Worker::runAll();
     }
