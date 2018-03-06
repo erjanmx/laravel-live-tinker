f082b1b1b4117d389c4cba729d3aaa69e83da6f0
erjanmx <erjanmx@gmail.com>
2018-03-06 17:36:00 +0300

    Add keyboard command
    Check for status code

--- src/resources/assets/js/app.js
+++ src/resources/assets/js/app.js
@@ -6,11 +6,19 @@ editor.on('change', function () {
     localStorage.setItem('live-tinker', editor.getValue());
 });
 
+// Add keyboard commands
 editor.commands.addCommand({
     name: 'runAll',
     bindKey: {win: 'Ctrl-Enter',  mac: 'Command-Enter'},
     exec: function(editor) {
         sendCode(editor.getValue())
+    }
+});
+editor.commands.addCommand({
+    name: 'runSelected',
+    bindKey: {win: 'Ctrl-Shift-Enter',  mac: 'Command-Shift-Enter'},
+    exec: function(editor) {
+        sendCode(editor.getSelectedText())
     }
 });
 
@@ -19,10 +27,15 @@ function sendCode(text) {
     xhr.open('POST', '/live-tinker/ajax');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function() {
-        document.Response.document.body.innerHTML = xhr.responseText;
+        var response = xhr.responseText;
+        if (xhr.status === 200) {
+            response = '<pre>' + response + '</pre>';
+        }
+
+        document.Response.document.body.innerHTML = response;
         changeButtonsState(false);
     };
-    xhr.send('code=' + encodeURIComponent(text));
+    xhr.send('c=' + encodeURIComponent(text));
 
     changeButtonsState(true);
     document.Response.document.body.innerHTML = '';
