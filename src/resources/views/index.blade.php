2f39ebb94b14c4f9c0c879183b40533232e15d8e
Erjan Kalybek <erjanmx@gmail.com>
2018-03-12 13:47:20 +0300

    Update page title

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>LiveTinker</title>
+    <title>Live-tinker</title>
     <link rel="stylesheet" href="/vendor/live-tinker/css/app.css">
 </head>
 <body>
